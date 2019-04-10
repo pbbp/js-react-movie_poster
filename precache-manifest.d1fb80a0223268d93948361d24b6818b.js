ca50bf7248733518949270c6417038f72a014278
Coy <patrick.bryan.peralta@gmail.com>
2019-04-10 16:49:51 -0400

    Updates

---
+++ precache-manifest.d1fb80a0223268d93948361d24b6818b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ead6017babc56ba809bd",
+    "url": "/js-react-movie_poster/static/css/main.c801cd45.chunk.css"
+  },
+  {
+    "revision": "ead6017babc56ba809bd",
+    "url": "/js-react-movie_poster/static/js/main.90e88c5f.chunk.js"
+  },
+  {
+    "revision": "148cd11c160d73fbe15e",
+    "url": "/js-react-movie_poster/static/js/runtime~main.60c0c4ba.js"
+  },
+  {
+    "revision": "17ebcc55a497cfb46121",
+    "url": "/js-react-movie_poster/static/js/2.0d2aad52.chunk.js"
+  },
+  {
+    "revision": "e506c4c67e8addc240bec9412c1e53b7",
+    "url": "/js-react-movie_poster/index.html"
+  }
+];
